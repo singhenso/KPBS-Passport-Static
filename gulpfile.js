33abd7b21a695b992546df223e17445d09c558ba
Gagandeep Singh <gsingh6@kpbs.org>
2023-03-15 06:57:34 -0700

    major update to include blocks and gulpfile and format html

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,9 @@ const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
 const uglify = require('gulp-uglify');
-const include = require('gulp-include');
+const fileinclude = require('gulp-file-include');
+const browserSync = require('browser-sync').create(); // Import BrowserSync
+const replace = require('gulp-replace');
 
 // Define paths
 const paths = {
@@ -32,7 +34,26 @@ function compileStyles() {
     .pipe(cleanCSS())
     .pipe(rename({ suffix: '.min' }))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest(paths.styles.dest));
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(browserSync.stream()); // Reload the CSS in the browser
+}
+
+// copy css:
+function copyCSS() {
+  return gulp.src('src/css/style.css')
+    .pipe(gulp.dest('dist/css'));
+}
+
+//include
+function includeHTML() {
+  return gulp.src(['index.html'])
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(replace(/dist\/img/g, './img')) // Replace "dist/img" with "/img"
+    .pipe(gulp.dest('dist'))
+    .pipe(browserSync.stream()); // Reload the HTML in the browser
 }
 
 // Minify and uglify JS
@@ -40,24 +61,39 @@ function minifyJS() {
   return gulp.src(paths.script.src)
     .pipe(uglify({ mangle: true }))
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest(paths.script.dest));
+    .pipe(gulp.dest(paths.script.dest))
+    .pipe(browserSync.stream()); // Reload the JS in the browser
 }
 
 // Optimize images
 function optimizeImages() {
   return gulp.src(paths.images.src)
-    .pipe(gulp.dest(paths.images.dest));
+    .pipe(gulp.dest(paths.images.dest))
+    .pipe(browserSync.stream()); // Reload the images in the browser
 }
 
 // Watch for changes to Less, JS, and image files
 function watch() {
+  browserSync.init({
+    server: {
+      baseDir: './dist',
+      serveStaticOptions: {
+        extensions: ['html']
+      }
+    }
+  }); // Initialize BrowserSync
+
   gulp.watch('src/less/**/*.less', compileStyles);
+  gulp.watch('src/css/**/*.css', gulp.series(copyCSS, browserSync.reload));
   gulp.watch('src/js/**/*.js', minifyJS);
   gulp.watch('src/img/**/*', optimizeImages);
+  gulp.watch(['index.html', 'src/blocks/**/*.html'], includeHTML);
 }
 
 // Export tasks
 exports.watch = watch;
 exports.minifyJS = minifyJS;
 exports.optimizeImages = optimizeImages;
-exports.default = gulp.series(gulp.parallel(compileStyles, minifyJS, optimizeImages), watch);
+exports.includeHTML = includeHTML;
+
+exports.default = gulp.series(gulp.parallel(compileStyles, copyCSS,minifyJS, optimizeImages, includeHTML), watch);
